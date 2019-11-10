49e2c335192e0a380feaee7d4609ef0860d593cc
schwartx <root.schwarz@protonmail.ch>
2019-11-10 09:09:47 +0800

    ball detection(excellent)

--- functions.py
+++ functions.py
@@ -8,78 +8,54 @@ SEARCHING = 30
 
 class PickBall:
     def __init__(self):
-        self.__Kp = [0.9, 1.2]
-        self.__Ki = array([.001, .001])
-        self.__Kd = array([.03, .03])
-
-        self.__last_time = 0
-        self.__last_err = array([0.,0.])
-        self.__err_sum = array([0.,0.])
-
-        self.__found = False
         self.__last_spd = array([0.,0.])
-
+        self.__reset_pid()
+        self.states = {'found': False, 'ready': False}
         self.clipper = array(SCALE_READY_CLIP)
-        self.__expect = sum(self.clipper)//2
-        self.__expect[1] += 5
-        self.__ready_for_clip = False
+        self.__expect = sum(self.clipper, axis=0)//2 + (0,5)
+        self.states = {'found': False, 'ready': False}
 
     def run(self, ball):
         if ball is not None:
-            if not self.__found:
-                self.__last_err = array([0,0])
-                self.__last_time = time()
-                self.__err_sum = array([0.,0.])
-                self.__found = True
+            if not self.states['found']:
+                self.__reset_pid()
+                self.states['found'] = True
 
             targ_p = ball[:2] + ball[-2:]//2
             if size(where((self.clipper[0] - targ_p)>0)[0]) == 0 and targ_p[0] < self.clipper[1][0]:
-                if not self.__ready_for_clip:
+                if not self.states['ready']:
                     self.clipper = array(SCALE_CLIPPER)
-                    self.__expect = sum(self.clipper)//2
-                    self.__expect[1] += 20
-                    self.__ready_for_clip = True
-                    self.__found = False
+                    self.__expect = sum(self.clipper, axis=0)//2 + (0,20)
+                    self.states = {'found': False, 'ready': True}
                     return 2, None
                 else:
                     return 1,None
-
-            # xerr, yerr
             err = self.__expect - targ_p
-            err_d = err - self.__last_err
-            time_d = time() - self.__last_time
+            err_d = (err - self.__last_err)/(time() - self.__last_time)
             self.__err_sum += err
-            self.__last_err = err
-            self.__last_time = time()
-            spd = self.__Kp*err + self.__Ki*self.__err_sum + self.__Kd*(err_d / time_d)
-            # lwspd, rwspd
-            # target is in the left screen
-            lwspd, rwspd = spd[1],spd[1]
-            if spd[0] > 0:
-                lwspd = spd[1] + spd[0]
-            elif spd[0] < 0:
-                rwspd = spd[1] - spd[0]
-            spd = lwspd, rwspd
-
+            self.__last_err,self.__last_time = err, time()
+            spd = sum((err, self.__err_sum, err_d) * self.__K, axis=0)
+            spd = (sum(spd), spd[1]) if spd[0]>0 else (spd[1], spd[1] - spd[0])
         else:
-            self.__found = False
-            if self.__last_err[0] > 0:
-                spd = (-SEARCHING,SEARCHING)
-            else:
-                spd = (SEARCHING,-SEARCHING)
+            self.states['found'] = False
+            spd = SEARCHING * array((-1,1) if self.__last_err[0]>0 else (1,-1))
 
         spd = around(spd).astype(int)
-
-        __should_run = sum(self.__last_spd - spd) == 0
+        should_run = sum(self.__last_spd - spd) == 0
         self.__last_spd = spd
-        if __should_run:
-            return 0,(int(spd[0]), int(spd[1]))
-        else:
-            return 0,None
+        return 0, (int(spd[0]), int(spd[1])) if should_run else None
+
+    def __reset_pid(self):
+        # Kp, Ki, Kd
+        self.__K  = array([[0.9, 1.2], [.001, .001], [.03, .03]])
+        self.__last_time = time()
+        # x,y
+        self.__last_err = array([0.,0.])
+        self.__err_sum = array([0.,0.])
 
 class GreenZone:
     def __init__(self):
-        self.__last_spd = 0
+        self.__last_spd = array([0.,0.])
         self.__reset_pid()
         self.__expect = array(SCALE_BOTTOM_CENTER)
         self.states = {'found': False, 'front': False, 'aimed': False}
